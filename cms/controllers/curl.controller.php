c008f807a58892bf58bfd5527101fc778a866c32
Gustavo Vázco <gustavovazco@gmail.com>
2026-03-10 00:46:56 -0600

    try to loggin

--- cms/controllers/curl.controller.php
+++ cms/controllers/curl.controller.php
@@ -13,7 +13,7 @@ class CurlController
 		$curl = curl_init();
 
 		curl_setopt_array($curl, array(
-			CURLOPT_URL => 'http://api-pos-cms.core-hub-plex.cloud/' . $url,
+			CURLOPT_URL => 'http://api/' . $url,
 			CURLOPT_RETURNTRANSFER => true,
 			CURLOPT_ENCODING => '',
 			CURLOPT_MAXREDIRS => 10,
